b137ff030c2a4559cd0dd1f0271e484477459c79
Victor Trigueros <@Victorc6>
2025-06-26 20:45:11 -0700

    Median function

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import math
+
+def median(input):
+  x = len(input)
+  mid = math.floor(x / 2)
+
+  if x % 2 == 0:
+    return (input[mid - 1] + input[mid]) / 2
+  else:
+    return input[mid]
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(median(my_list))
